e8553d156c346e40b74a2f2788a556ac1c751129
Jason-Ng00 <e0407783@u.nus.edu>
2023-02-15 16:24:32 +0800

    added powerbi

--- server.js
+++ server.js
@@ -60,10 +60,16 @@ app.post('/data', isAuth, (req, res) => {
     list = flattenJson(curr_data)
     data_list.push(list)
   }
+
+  fetch('https://api.powerbi.com/beta/5ba5ef5e-3109-4e77-85bd-cfeb0d347e82/datasets/60e4f48b-7f5b-4b04-aad2-47d76b8d901d/rows?key=Ztn0B0hFc%2FWfoePc2rXNcujNfiq70u24xTc1NF3VLmiq2CkYAMQgrQAiTHzMOwUqF1AkG%2FCjMl0TOXUuD5aK4w%3D%3D', {
+    method: 'POST',
+    body: data_list
+  })
+
   res.send(data_list);
 });
 
-app.post('/issue-data', isAuth, (req, res) => {
+app.post('/issue-data', isAuth, async (req, res) => {
   const content = {requestBody: req.body}
   console.log(req.body)
   if (!content) {
@@ -80,12 +86,7 @@ app.post('/issue-data', isAuth, (req, res) => {
     list = flattenJson(curr_data)
     data_list.push(list)
   }
-
-  fetch('https://api.powerbi.com/beta/5ba5ef5e-3109-4e77-85bd-cfeb0d347e82/datasets/60e4f48b-7f5b-4b04-aad2-47d76b8d901d/rows?key=Ztn0B0hFc%2FWfoePc2rXNcujNfiq70u24xTc1NF3VLmiq2CkYAMQgrQAiTHzMOwUqF1AkG%2FCjMl0TOXUuD5aK4w%3D%3D', {
-    method: 'POST',
-    body: data_list
-  })
-
+  
   res.send(data_list);
 });
 
